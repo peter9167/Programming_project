625d58643c0bcd4258df931573c51c94ac188415
Peter <peter9167@naver.com>
2022-07-11 19:46:19 +0900

    commit

--- C언어/test.c
+++ C언어/test.c
@@ -1,22 +1,37 @@
 #include <stdio.h>
 #include <string.h>
 
+typedef struct _Health {
+  /* 이름 */
+  char name[5];
+  /* 키 */
+  int height;
+  /* 몸무게 */
+  double weight;
+} Health;
+
+void strsort(char* h);
+
 int main()
 {
-    int i, a, len, len2;
-    double b;
-    char s[30];
-    char s1[90], s2[30];
+    Health h[5];
+    int i, j;
+    char tmp[5];
+    for(i = 0; i < 5; i++)
+    	scanf("%s %d %lf", h[i].name, &h[i].height, &h[i].weight);
     
-    scanf("%d %lf %s", &a, &b, s);
-    sprintf(s1, "%d", a);
-    sprintf(s2, "%.3f", b);
-    strcat(s1, s2);
-    strcat(s1, s);
-    len = strlen(s1);
-    if(len % 2 == 0) len2 = len / 2;
-    else len2 = (len / 2)+1;
-    for(i = 0; i<len2; i++) printf("%c", s1[i]);
-    printf("\n");
-    for(; i < len; i++) printf("%c", s1[i]);
+    for (i = 0; i < 5; i++) {
+	    for (j = 0; j < 5 - 1 - i; j++) {
+	        if (strcmp(h[j].name, h[j + 1].name) > 0) {
+	            tmp[j] = *h[j].name;
+	            *h[j].name = *h[j + 1].name;
+	            *h[j + 1].name = tmp[i];
+	        }
+	    }
+	}
+    
+    printf("name\n");
+    for(i = 0; i < 5; i++)
+    	printf("%s %d %.1f\n", h[i].name, h[i].height, h[i].weight);
 }
+
