362781486655663f2ae19616e08060c4c425b42d
Peter <peter9167@naver.com>
2022-07-11 16:55:15 +0900

    commit

--- C언어/test.c
+++ C언어/test.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h> 
-
 
 int main()
 {
-    char a[20];
-    int i;
+    int i, a, len, len2;
+    double b;
+    char s[30];
+    char s1[90], s2[30];
     
-    for(i = 0; i < 20; i++){
-    	scanf("%s", a);
-    	printf("%s", a);
-    	if(strcmp(a, "END")) break;
-	}
+    scanf("%d %lf %s", &a, &b, s);
+    sprintf(s1, "%d", a);
+    sprintf(s2, "%.3f", b);
+    strcat(s1, s2);
+    strcat(s1, s);
+    len = strlen(s1);
+    if(len % 2 == 0) len2 = len / 2;
+    else len2 = (len / 2)+1;
+    for(i = 0; i<len2; i++) printf("%c", s1[i]);
+    printf("\n");
+    for(; i < len; i++) printf("%c", s1[i]);
 }
