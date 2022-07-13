5144f34684f03ef6ba331d180e9263a93124a0da
Peter <peter9167@naver.com>
2022-07-13 13:24:36 +0900

    commit

--- C언어/test.c
+++ C언어/test.c
@@ -4,20 +4,35 @@ typedef struct
 {
     char name[20];
     int a, b, c;
+    int sum;
 }Result;
 
 
 int main()
 {
-    Result r;
-    int x;
-    float y;
+    Result r[10];
+    int n;
+    scanf("%d", &n);
     
-	scanf("%d %f %d %f", &f.x, &f.y, &f.x2, &f.y2);
-    
-    x = ((f.x + f.x2) / 2) + 5;
-    y = ((f.y + f.y2) / 2) - 4.5;
-    
-    printf("height : %dcm\n", x);
-    printf("weight : %.1fkg\n", y);
+    int i, j;
+    int sum[10];
+	for(i=0;i<n;i++){
+        scanf("%s %d %d %d",
+        r[i].name, &r[i].a, &r[i].b, &r[i].c);
+        r[i].sum = r[i].a+r[i].b+r[i].c;
+    }
+	
+	for(i = 0; i < n - 1; i++){
+		for(j = i + 1; j < n; j++){
+			if(r[i].sum < r[j].sum){
+				Result tmp = r[i];
+				r[i] = r[j];
+				r[j] = tmp;
+			}
+		}
+	}
+	
+	for(i = 0; i < n; i++) {
+		printf("%s %d %d %d %d\n", r[i].name, r[i].a, r[i].b, r[i].c, r[i].sum);
+	}
 }
