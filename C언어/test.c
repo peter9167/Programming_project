cd126bd51ff2e725d2bbbaeefab45fe7671ef8ba
Peter <peter9167@naver.com>
2022-07-20 08:54:22 +0900

    commit

--- C언어/test.c
+++ C언어/test.c
@@ -1,38 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-typedef struct
+int main(void)
 {
-    char name[20];
-    int a, b, c;
-    int sum;
-}Result;
-
-
-int main()
-{
-    Result r[10];
-    int n;
-    scanf("%d", &n);
-    
-    int i, j;
-    int sum[10];
-	for(i=0;i<n;i++){
-        scanf("%s %d %d %d",
-        r[i].name, &r[i].a, &r[i].b, &r[i].c);
-        r[i].sum = r[i].a+r[i].b+r[i].c;
-    }
-	
-	for(i = 0; i < n - 1; i++){
-		for(j = i + 1; j < n; j++){
-			if(r[i].sum < r[j].sum){
-				Result tmp = r[i];
-				r[i] = r[j];
-				r[j] = tmp;
-			}
-		}
-	}
-	
-	for(i = 0; i < n; i++) {
-		printf("%s %d %d %d %d\n", r[i].name, r[i].a, r[i].b, r[i].c, r[i].sum);
-	}
+	char s[100];
+	gets(s);
+	printf("%s", s);
 }
