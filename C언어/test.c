185bb7fda5a96f61d85d49786954435baaa54550
Peter <peter9167@naver.com>
2022-07-20 09:49:30 +0900

    commit

--- C언어/test.c
+++ C언어/test.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(void)
 {
-	char s[100];
-	gets(s);
-	printf("%s", s);
+	int n, i;
+	char a[10][100];
+	
+	scanf("%d", &n);
+	for(i = 0; i < n; i++)
+		fgets(a[i], 100, stdin);
+	for(; i > n; i--)
+		fputs(a[i], stdout); printf("\n");
 }
