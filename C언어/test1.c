ed0af665818c2753cd3f5382fc3ab6725c2d1516
Peter <peter9167@naver.com>
2022-07-21 08:33:04 +0900

    commit

--- C언어/test1.c
+++ C언어/test1.c
@@ -1,12 +1,27 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 
 int main(void)
 {
 	char s[100];
+	char max[100];
 	
 	fgets(s, 100, stdin);
-	printf("%s", s);
+	fprintf(stdout, "%d\n", strlen(s));
+	
+	char *ptr = strtok(s, " ");
+	strcpy(max, s);
+	
+    while (ptr != NULL)
+    {
+    	if(strlen(max) < strlen(ptr)){
+    		strcpy(max, ptr);
+		}
+        ptr = strtok(NULL, " ");
+    }
+    printf("%s\n", max);
+
+    return 0;
 }
 
 
