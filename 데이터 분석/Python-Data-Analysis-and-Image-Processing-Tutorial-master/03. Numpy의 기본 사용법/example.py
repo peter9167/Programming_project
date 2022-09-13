b6de46603b2ecb4bf802b0025a6ec5ffaea1555c
Peter <peter9167@naver.com>
2022-09-13 09:33:53 +0900

    commit

--- 데이터 분석/Python-Data-Analysis-and-Image-Processing-Tutorial-master/03. Numpy의 기본 사용법/example.py
+++ 데이터 분석/Python-Data-Analysis-and-Image-Processing-Tutorial-master/03. Numpy의 기본 사용법/example.py
@@ -23,3 +23,30 @@ print(array3)
 # 0부터 9까지 랜덤하기 / 초기화 된 배열 만들기
 array4 = np.random.randint(0, 10, (3, 3))
 print(array4)
+
+# 평균이 0이고, 표준편차가 1인 표준 정규를 띄는 배열 (표준정규분포)
+array5 = np.random.normal(0, 1, (3, 3))
+print(array5)
+
+# Numpy 배열 합치기
+array1 = np.array([1, 2, 3])
+array2 = np.array([4, 5, 6])
+array3 = np.concatenate([array1, array2])
+
+print(array3.shape) # 작성한 데이터의 크기
+print(array3) # 작성한 데이터 출력
+
+# Numpy 형태 바꾸기
+array1 = np.array([1, 2, 3, 4])
+array2 = array1.reshape((2, 2))
+
+print(array2)
+
+array3 = np.arange(4).reshape(1, 4)
+array4 = np.arange(8).reshape(2, 4)
+
+print(array3)
+print(array4)
+
+array3 = np.concatenate([array3, array4], axis=0)
+print(array3)
